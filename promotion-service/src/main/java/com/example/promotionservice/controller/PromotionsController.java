361ac47a78c60e9a4120bd13b8cd01f556faa455
Josias <50959958+SirJoc@users.noreply.github.com>
2021-10-29 03:54:00 -0500

    Update PromotionsController.java

--- promotion-service/src/main/java/com/example/promotionservice/controller/PromotionsController.java
+++ promotion-service/src/main/java/com/example/promotionservice/controller/PromotionsController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping(value = "/products")
+@RequestMapping(value = "/promotions")
 public class PromotionsController {
    @Autowired
    private PromotionService promotionService;
