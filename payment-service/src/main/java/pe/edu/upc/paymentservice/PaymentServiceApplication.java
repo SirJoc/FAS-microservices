9b5e62f7ce534a2d258f7f2338118d60135f9b6f
Maro <omaralonso27@gmail.com>
2021-11-05 12:22:54 -0500

    hot: Hystrix

--- payment-service/src/main/java/pe/edu/upc/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/pe/edu/upc/paymentservice/PaymentServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,6 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
---
+++ payment-service/src/main/java/pe/edu/upc/paymentservice/client/UserHystrixFallbackFactory.java
@@ -0,0 +1,26 @@
+package pe.edu.upc.paymentservice.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import pe.edu.upc.paymentservice.model.User;
+
+@Component
+public class UserHystrixFallbackFactory implements UserClient{
+    @Override
+    public ResponseEntity<User> fetchById(Long id) {
+        User user = User.builder()
+                .fullName("none")
+                .email("none")
+                .ruc("none").build();
+        return ResponseEntity.ok(user);
+    }
+
+    @Override
+    public ResponseEntity<User> updateUser(ResponseEntity<User> user, Long id) {
+        User user1 = User.builder()
+                .fullName("none")
+                .email("none")
+                .ruc("none").build();
+        return ResponseEntity.ok(user1);
+    }
+}
