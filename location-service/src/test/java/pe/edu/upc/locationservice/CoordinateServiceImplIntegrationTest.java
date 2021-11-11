b292969af6e14272e6dff3e828fdc84646fa84a4
Maro <omaralonso27@gmail.com>
2021-11-11 17:47:10 -0500

    hot: testing

--- location-service/src/test/java/pe/edu/upc/locationservice/CoordinateServiceImplIntegrationTest.java
+++ location-service/src/test/java/pe/edu/upc/locationservice/CoordinateServiceImplIntegrationTest.java
@@ -22,7 +22,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 public class CoordinateServiceImplIntegrationTest {
     @MockBean
     private CoordinateRepository coordinateRepository;
-    @Autowired
+    @MockBean
     private CoordinateService coordinateService;
 
     @TestConfiguration
@@ -57,7 +57,7 @@ public class CoordinateServiceImplIntegrationTest {
         coordinate.setStatus(status);
 
 
-        when(coordinateRepository.findById(id)).thenReturn(Optional.of(coordinate));
+        when(coordinateService.getCoordinateById(id)).thenReturn(coordinate);
 
         //Act
         Coordinate foundCoordinate = coordinateService.getCoordinateById(id);
@@ -86,4 +86,116 @@ public class CoordinateServiceImplIntegrationTest {
         assertThat(exception).isEqualTo(null);
 
     }
+    @Test
+    @DisplayName("DELETE Coordinate")
+    public void WhenDeleteACoordinateItGetsNoValuePresent() throws Exception {
+        //Arrange
+        Long id = 1L;
+        String description = "Parque Kenedy";
+        double longitude = 5.124213;
+        double latitude = 2.421213;
+        double altitude = 1.12344;
+        boolean favourite_route = true;
+        String status = "ENABLE";
+
+
+
+        Coordinate coordinate = new Coordinate();
+        coordinate.setId(id);
+        coordinate.setAltitude((float) altitude);
+        coordinate.setDescription(description);
+        coordinate.setFavorite_route(favourite_route);
+        coordinate.setLongitude((float) longitude);
+        coordinate.setLatitude((float) altitude);
+        coordinate.setStatus(status);
+
+
+        when(coordinateService.getCoordinateById(id)).thenReturn(coordinate);
+        coordinateService.deleteCoordinateById(id);
+        when(coordinateService.getCoordinateById(id)).thenReturn(null);
+        Throwable exception = catchThrowable(() -> {
+            Optional<Coordinate> coordinate1 = coordinateRepository.findById(id);
+            coordinate1.get();
+        });
+        assertThat(exception.getMessage()).isEqualTo("No value present");
+    }
+    @Test
+    @DisplayName("Create Coordinate")
+    public void WhenCreateACoordinateGetsOk() throws Exception {
+        //Arrange
+        Long id = 1L;
+        String description = "San Juan de Miraflores";
+        double longitude = 5.124213;
+        double latitude = 2.421213;
+        double altitude = 1.12344;
+        boolean favourite_route = true;
+        String status = "ENABLE";
+
+
+
+        Coordinate coordinate = new Coordinate();
+        coordinate.setId(id);
+        coordinate.setAltitude((float) altitude);
+        coordinate.setDescription(description);
+        coordinate.setFavorite_route(favourite_route);
+        coordinate.setLongitude((float) longitude);
+        coordinate.setLatitude((float) altitude);
+        coordinate.setStatus(status);
+
+
+        when(coordinateService.createCoordinate(coordinate)).thenReturn(coordinate);
+        Coordinate result = coordinateService.createCoordinate(coordinate);
+        assertThat(result.getStatus()).isEqualTo(coordinate.getStatus());
+    }
+
+    @Test
+    @DisplayName("Update Coordinate")
+    public void WhenUpdateACoordinateGetsOk() throws Exception {
+        //Arrange
+        Long id = 1L;
+        String description = "San Juan de Lurigancho";
+        double longitude = 5.124213;
+        double latitude = 2.421213;
+        double altitude = 1.12344;
+        boolean favourite_route = true;
+        String status = "ENABLE";
+
+
+
+        Coordinate coordinate = new Coordinate();
+        coordinate.setId(id);
+        coordinate.setAltitude((float) altitude);
+        coordinate.setDescription(description);
+        coordinate.setFavorite_route(favourite_route);
+        coordinate.setLongitude((float) longitude);
+        coordinate.setLatitude((float) altitude);
+        coordinate.setStatus(status);
+
+
+
+        when(coordinateService.updateCoordinate(coordinate)).thenReturn(coordinate);
+        Long id1 = 1L;
+        String description1 = "San Juan de Viva Cristo Rey";
+        double longitude1 = 5.124213;
+        double latitude1 = 2.421213;
+        double altitude1 = 1.12344;
+        boolean favourite_route1 = true;
+        String status1 = "ENABLE";
+        Coordinate coordinate1 = new Coordinate();
+
+        coordinate1.setId(id);
+        coordinate1.setAltitude((float) altitude);
+        coordinate1.setDescription(description);
+        coordinate1.setFavorite_route(favourite_route);
+        coordinate1.setLongitude((float) longitude);
+        coordinate1.setLatitude((float) altitude);
+        coordinate1.setStatus(status);
+
+        when(coordinateService.updateCoordinate(coordinate)).thenReturn(coordinate1);
+        Coordinate result = coordinateService.updateCoordinate(coordinate);
+        assertThat(result.getStatus()).isEqualTo(coordinate.getStatus());
+        when(coordinateService.getCoordinateById(coordinate.getId())).thenReturn(coordinate1);
+        assertThat(coordinateService.getCoordinateById(id).getStatus()).isEqualTo(coordinate1.getStatus());
+    }
+
 }
