cbee0497560fd1506fbd29b747638466d8c52824
Josias <50959958+SirJoc@users.noreply.github.com>
2021-11-11 15:19:07 -0500

    test: publicity testing
    update delete post and update

--- payment-service/src/test/java/pe/edu/upc/paymentservice/PublicityServiceImplIntegrationTest.java
+++ payment-service/src/test/java/pe/edu/upc/paymentservice/PublicityServiceImplIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pe.edu.upc.paymentservice.entities.Publicity;
+import pe.edu.upc.paymentservice.entities.Subscription;
 import pe.edu.upc.paymentservice.repository.PublicityRepository;
 import pe.edu.upc.paymentservice.service.PublicityService;
 import pe.edu.upc.paymentservice.service.impl.PublicityServiceImpl;
@@ -80,6 +81,80 @@ public class PublicityServiceImplIntegrationTest {
 
         //Assert
         assertThat(exception.getMessage()).isEqualTo("No value present");
+
+    }
+
+    @Test
+    @DisplayName("DELETE service")
+    public void WhenDeleteASubscriptionItGetsNoValuePresent() throws Exception {
+        //Arrange
+        Long id = 1L;
+        String message  = "Nuevos días";
+        int duration = 1;
+        Publicity publicity = new Publicity();
+        publicity.setId(id);
+        publicity.setMessage(message);
+        publicity.setDuration(duration);
+
+
+        when(publicityService.findById(id)).thenReturn(Optional.of(publicity));
+        publicityService.deleteById(id);
+        when(publicityService.findById(id)).thenReturn(Optional.empty());
+        Throwable exception = catchThrowable(() -> {
+            Optional<Publicity> publicity1 = publicityService.findById(id);
+            publicity1.get();
+        });
+        assertThat(exception.getMessage()).isEqualTo("No value present");
+    }
+
+
+    @Test
+    @DisplayName("Create service")
+    public void WhenCreateASubscriptionGetsOk() throws Exception {
+        //Arrange
+        Long id = 1L;
+        String message  = "Nuevos días";
+        int duration = 1;
+        Publicity publicity = new Publicity();
+        publicity.setId(id);
+        publicity.setMessage(message);
+        publicity.setDuration(duration);
+
+        when(publicityService.save(publicity)).thenReturn(publicity);
+        Publicity result = publicityService.save(publicity);
+        assertThat(result.getMessage()).isEqualTo(publicity.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("UPDATE service")
+    public void WhenUpdateASubcriptionGetsOk() throws Exception {
+        //Arrange
+        Long id = 1L;
+        String message  = "Nuevos días";
+        int duration = 1;
+        Publicity publicity = new Publicity();
+        publicity.setId(id);
+        publicity.setMessage(message);
+        publicity.setDuration(duration);
+
+        when(publicityService.save(publicity)).thenReturn(publicity);
+
+
+        Long id1 = 1L;
+        String message1  = "Buenas tardes";
+        int duration1 = 1;
+        Publicity publicity1 = new Publicity();
+        publicity1.setId(id);
+        publicity1.setMessage(message);
+        publicity1.setDuration(duration);
+
+
+
+
+        when(publicityService.save(publicity)).thenReturn(publicity1);
+        when(publicityService.findById(publicity.getId())).thenReturn(Optional.of(publicity1));
+        assertThat(publicityService.findById(id).get().getMessage()).isEqualTo(publicity1.getMessage());
 
     }
 }
