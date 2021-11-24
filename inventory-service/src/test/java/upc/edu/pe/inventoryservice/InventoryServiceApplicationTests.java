7c50d19614b43c3fe2a9c22a81c3f110bcbc5278
Josias <50959958+SirJoc@users.noreply.github.com>
2021-11-24 17:16:45 -0500

    test: inventory-service

--- inventory-service/src/test/java/upc/edu/pe/inventoryservice/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/upc/edu/pe/inventoryservice/InventoryServiceApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import upc.edu.pe.inventoryservice.entities.Product;
+import upc.edu.pe.inventoryservice.exception.ResourceNotFoundException;
 import upc.edu.pe.inventoryservice.repositories.CategoryRepository;
 import upc.edu.pe.inventoryservice.repositories.ProductRepository;
 import upc.edu.pe.inventoryservice.services.CategoryService;
@@ -20,6 +21,7 @@ import upc.edu.pe.inventoryservice.services.impl.ProductServiceImpl;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -29,7 +31,7 @@ class InventoryServiceApplicationTests {
 	@MockBean
 	private ProductRepository productRepository;
 
-	@Autowired
+	@MockBean
 	private ProductService productService;
 
 	@TestConfiguration
@@ -66,7 +68,7 @@ class InventoryServiceApplicationTests {
 		product.setDescription(description);
 		product.setCorporation(corporation);
 		product.setPrice(price);
-		when(productRepository.findById(id)).thenReturn(Optional.of(product));
+		when(productService.findById(id)).thenReturn(product);
 
 		// Act
 		Product foundProduct = productService.findById(id);
@@ -78,4 +80,104 @@ class InventoryServiceApplicationTests {
 		assertThat(foundProduct.getPrice()).isEqualTo(price);
 	}
 
+
+	@Test
+	@DisplayName("When findProductById With Invalid Id Then Returns RNFE")
+	public void whenFindProductByIdThenReturnsResourceNotFound() {
+		// Arrange
+		Long id = 1L;
+		String name = "PruebaName1";
+		String description = "PruebaDesc1";
+		String corporation = "PruebaCorp1";
+		Double price = 0.00;
+		Product product = new Product();
+		product.setName(name);
+		product.setDescription(description);
+		product.setCorporation(corporation);
+		product.setPrice(price);
+
+		//Act
+		when(productService.findById(id)).thenReturn(null);
+		assertThat(productService.findById(id)).isEqualTo(null);
+	}
+	@Test
+	@DisplayName("DELETE service")
+	public void WhenDeleteAProductItGetsNoValuePresent() throws Exception {
+		// Arrange
+		Long id = 1L;
+		String name = "PruebaName1";
+		String description = "PruebaDesc1";
+		String corporation = "PruebaCorp1";
+		Double price = 0.00;
+		Product product = new Product();
+		product.setName(name);
+		product.setDescription(description);
+		product.setCorporation(corporation);
+		product.setPrice(price);
+
+
+		when(productService.findById(id)).thenReturn(product);
+		productService.deleteById(id);
+		when(productService.findById(id)).thenReturn(null);
+		assertThat(productService.findById(id)).isEqualTo(null);
+	}
+
+
+	@Test
+	@DisplayName("Create service")
+	public void WhenCreateAProductGetsOk() throws Exception {
+		// Arrange
+		Long id = 1L;
+		String name = "PruebaName1";
+		String description = "PruebaDesc1";
+		String corporation = "PruebaCorp1";
+		Double price = 0.00;
+		Product product = new Product();
+		product.setName(name);
+		product.setDescription(description);
+		product.setCorporation(corporation);
+		product.setPrice(price);
+
+		when(productService.save(product)).thenReturn(product);
+		Product result = productService.save(product);
+		assertThat(result.getName()).isEqualTo(product.getName());
+	}
+
+
+	@Test
+	@DisplayName("UPDATE service")
+	public void WhenUpdateAProductGetsOk() throws Exception {
+		// Arrange
+		Long id = 1L;
+		String name = "PruebaName1";
+		String description = "PruebaDesc1";
+		String corporation = "PruebaCorp1";
+		Double price = 0.00;
+		Product product = new Product();
+		product.setId(id);
+		product.setName(name);
+		product.setDescription(description);
+		product.setCorporation(corporation);
+		product.setPrice(price);
+
+		when(productService.save(product)).thenReturn(product);
+
+
+		// Arrange
+		Long id1 = 1L;
+		String name1 = "PruebaName1";
+		String description1 = "PruebaDesc1";
+		String corporation1 = "PruebaCorp1";
+		Double price1 = 0.00;
+		Product product1 = new Product();
+		product1.setName(name1);
+		product1.setDescription(description1);
+		product1.setCorporation(corporation1);
+		product1.setPrice(price1);
+
+		when(productService.save(product)).thenReturn(product1);
+		when(productService.findById(product.getId())).thenReturn(product1);
+		assertThat(productService.findById(id).getName()).isEqualTo(product1.getName());
+
+	}
 }
